53bc67c6301aca1e87bff08b44cb3b7b667cb8fb
Ronen Amiel <ronen.amiel@gmail.com>
2020-10-23 22:36:58 +0300

    evaluating call expressions

--- src/eval.d.ts
+++ src/eval.d.ts
@@ -1,6 +1,7 @@
 import { Tokenize } from './tokenize';
 import {
   BooleanExpression,
+  CallExpression,
   Expression,
   IfExpression,
   NullExpression,
@@ -12,6 +13,7 @@ import {
 } from './parse';
 import type { Reverse, Tail, Unshift } from './arrayUtils';
 import { AnalyzeSequence } from './analyze';
+import { ConcatStrings, Cast } from './stringUtils';
 
 type Eval<E extends Expression> = E extends NullExpression
   ? null
@@ -23,7 +25,23 @@ type Eval<E extends Expression> = E extends NullExpression
   ? V
   : E extends IfExpression<infer P, infer T, infer E>
   ? EvalIf<P, T, E>
+  : E extends CallExpression<infer N, infer P>
+  ? EvalCall<N, P>
   : '';
+
+type EvalCall<N extends Expression, P extends Array<Expression>> = EvalSequence<
+  P
+> extends infer G
+  ? N extends VariableExpression<'concat'>
+    ? ConcatStrings<Cast<G, Array<any>>[0], Cast<G, Array<any>>[1]>
+    : never
+  : never;
+
+type EvalSequence<
+  //
+  E extends Array<any>,
+  R extends Array<any> = []
+> = E extends [] ? Reverse<R> : EvalSequence<Tail<E>, Unshift<R, Eval<E[0]>>>;
 
 type EvalIf<
   P extends Expression,
@@ -32,7 +50,7 @@ type EvalIf<
   R = Eval<P>
 > = R extends false ? Eval<E> : Eval<T>;
 
-type Tokens = Tokenize<'(add 1 2 3)'>;
+type Tokens = Tokenize<'(concat "a" "b")'>;
 type Parsed = ParseSequence<Tokens>;
 type Analyzed = AnalyzeSequence<Parsed>;
-type Result = Eval<Analyzed[0]>;
+type Result = EvalSequence<Analyzed>;
