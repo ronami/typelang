ad90b0b013bef55365fe7e48d8bdb17a766a56a9
Ronen Amiel <ronen.amiel@gmail.com>
2020-10-23 15:16:55 +0300

    parse a sequence of tokens

--- src/parse.d.ts
+++ src/parse.d.ts
@@ -1,4 +1,4 @@
-import { Reverse, Unshift } from './arrayUtils';
+import { Reverse, Tail, Unshift } from './arrayUtils';
 import { EatFirstChar, FirstChar, ConcatStrings } from './stringUtils';
 import { isNumberCharacter, isLetterCharacter } from './numberUtils';
 import {
@@ -13,21 +13,35 @@ export type Parse<
   T extends Array<Token<any>>,
   F extends Token<any> = T[0]
 > = F extends NumberToken<infer V>
-  ? {
-      type: 'NumberLiteral';
-      value: V;
-    }
+  ? [
+      {
+        type: 'NumberLiteral';
+        value: V;
+      },
+      Tail<T>,
+    ]
   : F extends StringToken<infer V>
-  ? {
-      type: 'StringLiteral';
-      value: V;
-    }
+  ? [
+      {
+        type: 'StringLiteral';
+        value: V;
+      },
+      Tail<T>,
+    ]
   : F extends ParenToken<'('>
-  ? {
-      type: 'CallExpression';
-      name: '(';
-      params: [];
-    }
+  ? [
+      {
+        type: 'CallExpression';
+        name: '(';
+        params: [];
+      },
+    ]
   : T;
 
-type R = Parse<Tokenize<'(123)'>>;
+type ParseSequence<
+  T extends Array<Token<any>>,
+  R extends Array<any> = [],
+  P extends Array<any> = Parse<T>
+> = T extends [] ? Reverse<R> : ParseSequence<P[1], Unshift<R, P[0]>>;
+
+type R = ParseSequence<Tokenize<'123 456 "hello"'>>;
