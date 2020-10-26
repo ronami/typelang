2a14fd39eb3f6c356cf511430d0d90cde740153d
Ronen Amiel <ronen.amiel@gmail.com>
2020-10-26 22:23:11 +0200

    add some spacing for clearity

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -3,9 +3,13 @@ import type { EatFirstChar, FirstChar, ConcatStrings } from './stringUtils';
 import type { Numbers, Symbols } from './generalUtils';
 
 export type ParenToken<V> = { type: 'paren'; value: V };
+
 export type NumberToken<V> = { type: 'number'; value: V };
+
 export type StringToken<V> = { type: 'string'; value: V };
+
 export type SymbolToken<V> = { type: 'symbol'; value: V };
+
 export type Token<V> =
   | ParenToken<V>
   | NumberToken<V>
